c831b59d865fe7a5364cea9fb61d8ab5ffc233af
Behdad Keynejad <behdad.keynejad@gmail.com>
2018-06-12 18:40:50 +0430

    AAAAARGRRRGGHHHHH

--- SNPUtilities/Classes/Extensions/Bundle.swift
+++ SNPUtilities/Classes/Extensions/Bundle.swift
@@ -8,24 +8,3 @@
 
 import Foundation
 
-extension Bundle {
-    func load<T>(_ nibName: String = String(describing: T.self)) -> T {
-        return loadNibNamed(nibName, owner: nil, options: nil)?.first as! T
-    }
-
-    func info(for key: String) -> String! {
-        return (infoDictionary?[key] as! String).replacingOccurrences(of: "\\", with: "")
-    }
-
-    func read(fileName: String, type: String) -> String? {
-        if let path = self.path(forResource: fileName, ofType: type) {
-            do {
-                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
-                return String(data: data, encoding: .utf8)
-            } catch {
-                return nil
-            }
-        }
-        return nil
-    }
-}
--- SNPUtilities/Classes/SNPErrorDomain.swift
+++ SNPUtilities/Classes/SNPErrorDomain.swift
@@ -8,14 +8,37 @@
 
 import Foundation
 
+extension Bundle {
+    func load<T>(_ nibName: String = String(describing: T.self)) -> T {
+        return loadNibNamed(nibName, owner: nil, options: nil)?.first as! T
+    }
+    
+    func info(for key: String) -> String! {
+        return (infoDictionary?[key] as! String).replacingOccurrences(of: "\\", with: "")
+    }
+    
+    func read(fileName: String, type: String) -> String? {
+        if let path = self.path(forResource: fileName, ofType: type) {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                return String(data: data, encoding: .utf8)
+            } catch {
+                return nil
+            }
+        }
+        return nil
+    }
+}
+
+
 public struct SNPErrorDomain {
     private static let prefix: String = {
-//        if Bundle.main.info(for: kCFBundleIdentifierKey! as String) == nil {
-//            return "test.bundle.error"
-//        } else {
-//            return Bundle.main.info(for: kCFBundleIdentifierKey! as String) + ".error"
-//        }
-        return "error"
+        if Bundle.main.info(for: kCFBundleIdentifierKey! as String) == nil {
+            return "test.bundle.error"
+        } else {
+            return Bundle.main.info(for: kCFBundleIdentifierKey! as String) + ".error"
+        }
+//        return "error"
     }()
     
     static let generic = prefix
