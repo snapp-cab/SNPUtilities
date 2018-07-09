f517871624b1018af111e2debcba125e0f83ae14
farhad <jebelli.farhad@gmail.com>
2018-07-09 14:36:48 +0430

    refactor uiviewcontroller extention

--- SNPUtilities/Classes/Extensions/UIViewController.swift
+++ SNPUtilities/Classes/Extensions/UIViewController.swift
@@ -11,12 +11,8 @@ extension UIViewController {
     public func embed(childViewController: UIViewController, in containerView: UIView) {
         self.addChildViewController(childViewController)
         childViewController.didMove(toParentViewController: self)
-        containerView.addSubview(childViewController.view)
         
-        childViewController.view.translatesAutoresizingMaskIntoConstraints = false
-        containerView.topAnchor.constraint(equalTo: childViewController.view.topAnchor).isActive = true
-        containerView.leadingAnchor.constraint(equalTo: childViewController.view.leadingAnchor).isActive = true
-        containerView.bottomAnchor.constraint(equalTo: childViewController.view.bottomAnchor).isActive = true
-        containerView.trailingAnchor.constraint(equalTo: childViewController.view.trailingAnchor).isActive = true
+        containerView.addExpletiveSubView(view: containerView)
+    
     }
 }
