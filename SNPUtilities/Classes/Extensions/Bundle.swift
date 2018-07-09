27c16ee6c02ef5760e47776995e98aa55ad2fd68
farhad <jebelli.farhad@gmail.com>
2018-07-09 21:39:42 +0430

    fix owner bug

--- SNPUtilities/Classes/Extensions/Bundle.swift
+++ SNPUtilities/Classes/Extensions/Bundle.swift
@@ -9,7 +9,7 @@ import Foundation
 
 extension Bundle {
     public func load<T>(_ nibName: String = String(describing: T.self), owner: Any? = nil) -> T {
-        return loadNibNamed(nibName, owner: nil, options: nil)?.first as! T
+        return loadNibNamed(nibName, owner: owner, options: nil)?.first as! T
     }
     
     public func info(for key: String) -> String! {
