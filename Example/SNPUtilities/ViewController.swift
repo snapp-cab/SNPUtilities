7210216ef8c215fc4f09e73a9d6752c3f9dddd8b
farhad <jebelli.farhad@gmail.com>
2018-08-01 15:10:48 +0430

    add SNPKeybordHeightConstraint

--- Example/SNPUtilities/ViewController.swift
+++ Example/SNPUtilities/ViewController.swift
@@ -14,13 +14,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        SNPUtilities.clearTempDirectory()
+        //SNPUtilities.clearTempDirectory()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
 
+    }
 }
+
 
