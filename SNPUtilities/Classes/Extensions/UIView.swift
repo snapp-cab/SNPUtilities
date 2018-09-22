59b56b2419224b4397c6d0005cb94340463f458a
arashzjahangiri <arashzjahangiri@gmail.com>
2018-09-22 15:26:05 +0330

    Email validation + Shake UIView

--- SNPUtilities/Classes/Extensions/UIView.swift
+++ SNPUtilities/Classes/Extensions/UIView.swift
@@ -16,4 +16,17 @@ extension UIView {
         view.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
         view.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
     }
+    
+    public func shake() {
+        func shake(count : Float = 4,for duration : TimeInterval = 0.5,withTranslation translation : Float = 5) {
+            let animation : CABasicAnimation = CABasicAnimation(keyPath: "transform.translation.x")
+            animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+            animation.repeatCount = count
+            animation.duration = duration/TimeInterval(animation.repeatCount)
+            animation.autoreverses = true
+            animation.fromValue = NSValue(cgPoint: CGPoint(x: CGFloat(-translation), y: self.center.y))
+            animation.toValue = NSValue(cgPoint: CGPoint(x: CGFloat(translation), y: self.center.y))
+            layer.add(animation, forKey: "shake")
+        }
+    }
 }
